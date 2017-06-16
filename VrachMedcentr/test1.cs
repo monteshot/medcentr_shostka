d7b01462d859820b69b36f52e60f8411400d3261
Сергей Кущ <faqreg@gmail.com>
2017-06-16 15:49:05 +0300

    azazaz

---
+++ VrachMedcentr/test1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_Hospital;
+
+namespace VrachMedcentr
+{
+    class test1 : INotifyPropertyChanging
+    {
+        public string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChanged("Title"); }
+
+        }
+
+     
+
+
+        //  public event PropertyChangedEventHandler PropertyChanged;
+        // public event PropertyChangingEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanging != null)
+                PropertyChanging(this, new PropertyChangingEventArgs(prop));
+            
+          //  MainWindow mw = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+        }
+    }
+}
